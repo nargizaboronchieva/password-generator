d6ea8f4a17c7d42bad20ce3cd85eae787ddec0d8
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-05-25 20:38:07 -0400

    Final update on script.js file

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
     var confirmLength = (prompt ("How many characters would you like your password to contain?")); 
     // Loop if answer is outside the parameters 
     while (confirmLength <= 7 || confirmLength >= 129 ) { 
-    alert ("Password length must be between 8-129 characters. Try again" ) ; 
+    alert ("Password length must be between 8-128 characters. Try again" ) ; 
     var confirmLength = (prompt ("How many characters would you like your password to contain?")) ; 
     } 
     // Repeat back how many characters the user will have 
